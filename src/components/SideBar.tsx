25c55bb9fe19529bb632087eb3e719ef3014d070
Sonda <genoino.jr@Gmail.com>
2022-01-19 16:02:25 -0300

    Criando componente Content e SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,19 @@
-export function SideBar() {
+interface GenreResponseProps {
+  genre: {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  };
+}
+
+export function SideBar(props: GenreResponseProps) {
   // Complete aqui
+
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {props.genre.title}</span>
+      </span>
+    </header>
+  );
 }
